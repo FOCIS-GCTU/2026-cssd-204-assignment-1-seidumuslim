c28b7718baa173e3808499e8681fd489beb3b4f7
Seidu Muslim <vybzcarter2005@gmail.com>
2026-05-31 17:51:25 -0800

    Add files via upload
    Assignment 1

--- initials.py
+++ initials.py
@@ -1,7 +1,40 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Muslim Seidu
+# STUDENT ID:  2425402300
+# Email: 2425402300@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Muslim Seidu, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
+    # Print my initials in small and large stylized letters
 
+    print()
+    print("...MSD")
+    print()
 
+    print("...MM........MM............SSSSSSSSS.......DDDDDDDDDD......")
+    print("...MM........MM...........SSSSSSSSSS.......DD.......DD.....")
+    print("...MMM......MMM..........SSSS..............DD........DD....")
+    print("...MM.MM..MM.MM.........SSSS...............DD........DD....")
+    print("...MM..MMMM..MM......... SSSS..............DD........DD....")
+    print("...MM...MM...MM.......... SSSSSSSS.........DD........DD....")
+    print("...MM........MM............... SSSS........DD........DD....")
+    print("...MM........MM................SSSS........DD........DD....")
+    print("...MM........MM..........SSSSSSSSSS....... DD........DD....")
+    print("...MM........MM...**.....SSSSSSSSS....**...DDDDDDDDDD......")
+
+    print()
 
 main()
+
+
+
+
+
+
+
